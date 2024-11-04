ca0498bedea95994087dbea2289e4961c1f3132d
manuel-garcin <manuel.garcin@laplateforme.io>
2024-11-04 17:57:40 +0100

    Update commands-git.py

--- commands-git.py
+++ commands-git.py
@@ -10,4 +10,8 @@ git commit -m "commentaire"
 git push
 
 #commande pour effectuer un pull
-?
+créer une nouvelle branche, apporter les modifications au fichier,
+merge, accepter les modifs sur la branche principale
+une fois dans le terminal de commande faire un
+git push pour mettre à jour le fichier
+avec les modifications apportés sur GitHub
