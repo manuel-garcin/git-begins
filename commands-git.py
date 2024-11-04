16ba506a04c93b8fe2e52da69d2d2954b0fa7867
Manuel-Garcin <manuel.garcin@laplateforme.io>
2024-11-04 13:58:06 +0100

    commandes push

---
+++ commands-git.py
@@ -0,0 +1,10 @@
+#pour cloner un repository
+#dans un terminal, par exemple VS Code
+git clone "adresse du repository"
+
+#on ajoute le code souhaité
+
+#on envoie le code sur GitHub
+git add .
+git commit -m "commentaire"
+git push
